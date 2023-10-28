9232e335509ab235ccb8888adb4342a780354de2
manuelcampagnolo <mlc@isa.ulisboa.pt>
2023-10-28 06:31:46 +0100

    secant added

--- myfunctions.py
+++ myfunctions.py
@@ -7,23 +7,88 @@ def deg2rad(x):
 def rad2deg(x):
     return 180*x/pi
 
-# Square root function using bissection method
-# Input: number
-def  mysqrt(x):
-    if x<0:
+def mysqrt(y):
+    N=100
+    if y==0:
+        return 0.0
+    if y<0:
         return 'NaN'
-    else:
-        tol=10**(-10)
-        min=0
-        max=x+1
-        mid=(min+max)/2
-        while abs(mid*mid-x)>tol:
-            if mid*mid>x:
-                max=mid
-            else:
-                min=mid
-            mid=(min+max)/2
-        return mid
+    
+    def f(x):
+        return x*x-y
+    
+    a=0
+    b=y+1
+
+    '''Approximate solution of f(x)=0 on interval [a,b] by the secant method.
+
+    Parameters
+    ----------
+    f : function
+        The function for which we are trying to approximate a solution f(x)=0.
+    a,b : numbers
+        The interval in which to search for a solution. The function returns
+        None if f(a)*f(b) >= 0 since a solution is not guaranteed.
+    N : (positive) integer
+        The number of iterations to implement.
+
+    Returns
+    -------
+    m_N : number
+        The x intercept of the secant line on the the Nth interval
+            m_n = a_n - f(a_n)*(b_n - a_n)/(f(b_n) - f(a_n))
+        The initial interval [a_0,b_0] is given by [a,b]. If f(m_n) == 0
+        for some intercept m_n then the function returns this solution.
+        If all signs of values f(a_n), f(b_n) and f(m_n) are the same at any
+        iterations, the secant method fails and return None.
+
+    Examples
+    --------
+    >>> f = lambda x: x**2 - x - 1
+    >>> secant(f,1,2,5)
+    1.6180257510729614
+    '''
+    if f(a)*f(b) >= 0:
+        print("Secant method fails.")
+        return 'NaN'
+    a_n = a
+    b_n = b
+    for n in range(1,N+1):
+        m_n = a_n - f(a_n)*(b_n - a_n)/(f(b_n) - f(a_n))
+        f_m_n = f(m_n)
+        if f(a_n)*f_m_n < 0:
+            a_n = a_n
+            b_n = m_n
+        elif f(b_n)*f_m_n < 0:
+            a_n = m_n
+            b_n = b_n
+        elif f_m_n == 0:
+            print("Found exact solution.")
+            return m_n
+        else:
+            print("Secant method fails.")
+            return None
+    return a_n - f(a_n)*(b_n - a_n)/(f(b_n) - f(a_n))
+
+#print(sqrt_secant(0.0001,100))
+
+# # Square root function using bissection method
+# # Input: number
+# def  mysqrt(x):
+#     if x<0:
+#         return 'NaN'
+#     else:
+#         tol=10**(-10)
+#         min=0
+#         max=x+1
+#         mid=(min+max)/2
+#         while abs(mid*mid-x)>tol:
+#             if mid*mid>x:
+#                 max=mid
+#             else:
+#                 min=mid
+#             mid=(min+max)/2
+#         return mid
 
 # McLaurin approximation for cosine
 # Input: angle in degrees
@@ -51,23 +116,87 @@ def mysin(x):
 
 #print(mysin(30),mysin(45),mysin(-45), mysin(-135))
 
-# Arcsin using bissection method
-# input: number
-# output: angle in decimal degrees
-def myarcsin(x):
-    if not -1 <= x <= 1:
+
+def myarcsin(y):
+    N=100
+    if y<-1 or y>1:
         return 'NaN'
-    tol=10**(-13)
-    min= -90
-    max= 90
-    mid=(min+max)/2
-    while abs(mysin(mid)-x)>tol:
-        if mysin(mid)>x:
-            max=mid
+    
+    def f(x):
+        return mysin(x)-y
+    
+    a=-90
+    b=90
+
+    '''Approximate solution of f(x)=0 on interval [a,b] by the secant method.
+
+    Parameters
+    ----------
+    f : function
+        The function for which we are trying to approximate a solution f(x)=0.
+    a,b : numbers
+        The interval in which to search for a solution. The function returns
+        None if f(a)*f(b) >= 0 since a solution is not guaranteed.
+    N : (positive) integer
+        The number of iterations to implement.
+
+    Returns
+    -------
+    m_N : number
+        The x intercept of the secant line on the the Nth interval
+            m_n = a_n - f(a_n)*(b_n - a_n)/(f(b_n) - f(a_n))
+        The initial interval [a_0,b_0] is given by [a,b]. If f(m_n) == 0
+        for some intercept m_n then the function returns this solution.
+        If all signs of values f(a_n), f(b_n) and f(m_n) are the same at any
+        iterations, the secant method fails and return None.
+
+    Examples
+    --------
+    >>> f = lambda x: x**2 - x - 1
+    >>> secant(f,1,2,5)
+    1.6180257510729614
+    '''
+    if f(a)*f(b) >= 0:
+        print("Secant method fails.")
+        return 'NaN'
+    a_n = a
+    b_n = b
+    for n in range(1,N+1):
+        m_n = a_n - f(a_n)*(b_n - a_n)/(f(b_n) - f(a_n))
+        f_m_n = f(m_n)
+        if f(a_n)*f_m_n < 0:
+            a_n = a_n
+            b_n = m_n
+        elif f(b_n)*f_m_n < 0:
+            a_n = m_n
+            b_n = b_n
+        elif f_m_n == 0:
+            print("Found exact solution.")
+            return m_n
         else:
-            min=mid
-        mid=(min+max)/2
-    return mid
+            print("Secant method fails.")
+            return None
+    return a_n - f(a_n)*(b_n - a_n)/(f(b_n) - f(a_n))
+
+#print(arcsin_secant(0.5,100), arcsin_secant(-0.5,100))
+
+# # Arcsin using bissection method
+# # input: number
+# # output: angle in decimal degrees
+# def myarcsin(x):
+#     if not -1 <= x <= 1:
+#         return 'NaN'
+#     tol=10**(-13)
+#     min= -90
+#     max= 90
+#     mid=(min+max)/2
+#     while abs(mysin(mid)-x)>tol:
+#         if mysin(mid)>x:
+#             max=mid
+#         else:
+#             min=mid
+#         mid=(min+max)/2
+#     return mid
 
 # input: string (prompt to user), float (minimum value for input), float (maximum value for input)
 # output: float (user's provided value between minimum and maximum)
