ee5dfe2ae91c40d5afa4dd99efca72e470c77251
Manuel Campagnolo <mlc@isa.ulisboa.pt>
2024-02-28 11:42:18 +0000

    Update floyd_negative_cycles_foobar.py

--- floyd_negative_cycles_foobar.py
+++ floyd_negative_cycles_foobar.py
@@ -1,20 +1,13 @@
+# returns costs of shortest paths in at most n steps
 def floyd_warshall(graph):
     n = len(graph)
-    dist = [[float('inf')] * n for _ in range(n)]
-    
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                dist[i][j] = 0
-            elif graph[i][j] != float('inf'):
-                dist[i][j] = graph[i][j]
-    
+    # initialize dist
+    dist = graph.copy()
+    # compute shortest distances after n steps
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                if dist[i][k] != float('inf') and dist[k][j] != float('inf'):
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-    
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
     return dist
 
 def are_there_negative_cycles(graph):
@@ -30,11 +23,11 @@ def are_there_negative_cycles(graph):
 if __name__ == "__main__":
     # Example cost matrix A
     test = [[0, 3, 4, 6],
-        [3, 0, 1, 7],
-        [2, 3, 4, -2],
-        [-3, 3, 4, 8]]
+            [3, 0, 1, 7],
+            [2, 3, 4, -2],
+            [-3, 3, 4, 8]]
     
-    # Calculate shortest paths
+    # Calculate shortest paths after n steps
     print(floyd_warshall(test))
 
     # are there negative cycles:
